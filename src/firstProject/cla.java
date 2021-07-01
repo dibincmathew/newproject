37937eb1e3ed38b7739e576553ae8bade3da9580
dibin <dibincmathew@gmail.com>
2021-07-01 08:25:27 +0530

    first commit

---
+++ src/firstProject/cla.java
@@ -0,0 +1,29 @@
+package firstProject;
+
+public class cla {
+	
+		public static void main(String[] args)
+		{
+		int i,j;
+		int n=4;
+		int k=1;
+		
+        
+		for(i=0;i<5;i++)
+		{
+			
+		for(j=0;j<n;j++)
+		{
+		System.out.print("*");
+		}
+		
+		
+		
+		System.out.print(k);
+		
+
+		}
+		
+}
+}
+
